ec1701e2a907415d40b3a6bba4ea9650880b975c
Heinz N. Gies <heinz@licenser.net>
2016-08-29 21:25:11 +0200

    Remove get_fun

--- src/dqe.erl
+++ src/dqe.erl
@@ -245,35 +245,36 @@ extract_gets(#{op := get, args := [_, _,_, B, M]}) ->
 translate({calc, [], G}) ->
     translate(G);
 
-translate({calc,
-           [#{op := fcall,
-              resolution := R,
-              args :=
-                  #{
-                    mod := Mod,
-                state := State
-              }}],
-           #{op := get, args := Args}}) ->
-    G1 = {dqe_get_fun, [Mod, State] ++ Args},
-    {ok, R, G1};
+%% Sadly this isn't really working, leave it in here.
+%% translate({calc,
+%%            [#{op := fcall,
+%%               resolution := R,
+%%               args :=
+%%                   #{
+%%                     mod := Mod,
+%%                 state := State
+%%               }}],
+%%            #{op := get, args := Args}}) ->
+%%     G1 = {dqe_get_fun, [Mod, State] ++ Args},
+%%     {ok, R, G1};
 
-translate({calc,
-           [#{op := fcall,
-              args := #{
-                    mod := Mod,
-               state := State
-              }} | Aggrs],
-           #{op := get, args := Args}}) ->
-    FoldFn = fun(#{op := fcall,
-                   args := #{
-                         mod := ModX,
-                     state := StateX
-                    }}, Acc) ->
-                     {dqe_fun_flow, [ModX, StateX, Acc]}
-             end,
-    #{resolution := R} = lists:last(Aggrs),
-    G1 = {dqe_get_fun, [Mod, State] ++ Args},
-    {ok, R, lists:foldl(FoldFn, G1, Aggrs)};
+%% translate({calc,
+%%            [#{op := fcall,
+%%               args := #{
+%%                     mod := Mod,
+%%                state := State
+%%               }} | Aggrs],
+%%            #{op := get, args := Args}}) ->
+%%     FoldFn = fun(#{op := fcall,
+%%                    args := #{
+%%                          mod := ModX,
+%%                      state := StateX
+%%                     }}, Acc) ->
+%%                      {dqe_fun_flow, [ModX, StateX, Acc]}
+%%              end,
+%%     #{resolution := R} = lists:last(Aggrs),
+%%     G1 = {dqe_get_fun, [Mod, State] ++ Args},
+%%     {ok, R, lists:foldl(FoldFn, G1, Aggrs)};
 
 %% TODO we can do this better!
 
