b6a0fa8610d9562c263571647d71744e0dc20d38
Heinz N. Gies <heinz@licenser.net>
2017-04-11 14:42:08 +0200

    libbump

--- src/dqe_span.erl
+++ src/dqe_span.erl
@@ -1,32 +1,19 @@
 -module(dqe_span).
 
--export([id/1, start/2, stop/0, tag/2, log/1]).
+-export([start/2, stop/0, tag/2, log/1]).
 
--define(IF_SPAN(Code),
-        case otter:span_pget() of
-            undefined ->
-                ok;
-            _ ->
-                Code
-        end).
-
-%% Random 64 bit integer.
-id(undefined) ->
-    undefined;
-
-id(_ParentSpan) ->
-    otter_lib:id().
 
 start(_, undefined) ->
     ok;
+
 start(Name, TraceID) ->
-    otter:span_pstart(Name, TraceID).
+    ottersp:start(Name, TraceID).
 
 stop() ->
-    ?IF_SPAN(otter:span_pend()).
+    ottersp:finish().
 
 tag(Key, Value) ->
-    ?IF_SPAN(otter:span_ptag(Key, Value, "dqe")).
+    ottersp:tag(Key, Value, "dqe").
 
 log(Text) ->
-    ?IF_SPAN(otter:span_plog(Text, "dqe")).
+    ottersp:log(Text, "dqe").
